652c6deb15d14eb2381a5a8ef7e24c8ddd51c129
Hitesh Kumar Roy <hiteshroy011@gmail.com>
2025-09-13 22:20:00 +0530

    complete auth for user

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -43,3 +43,39 @@ return res.status(500).json({message:`signup error ${error}`});
   }
 }
 
+export const login=async(req,res)=>{
+  try{
+    const {email,password}=req.body
+    let user=await User.findOne({email});
+    if(!user){
+      return res.status(404).json({message:"user not found"});
+    }
+    const isCorrect=await bcrypt.compare(password,user.password);
+    if(!isCorrect){
+         return res.status(404).json({message:"incorrect password"});
+    }
+
+    let token=await gentoken(user._id);
+
+    req.cookie("token",token,{
+        httpOnly:true,
+  secure:false,
+  sameSite:"strict",
+  maxAge:7*24*60*60*1000
+    })
+    return res.status(200).json(user);
+  }catch(error){
+    return res.status(500).json({message:`login error ${error}`});
+
+  }
+}
+
+export const logOut=async(req,res)=>{
+  try{
+await res.clearCookie("token");
+return res.status(200).json({message:"logout successfully"});
+  }catch(error){
+    return res.status(500).json({message:`logout error ${error}`});
+    
+  }
+}
