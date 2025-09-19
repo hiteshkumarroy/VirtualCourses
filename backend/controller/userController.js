c5d1f96618f2f53c2219f4ae9419a11edf5b07f4
Hitesh Kumar Roy <hiteshroy011@gmail.com>
2025-09-19 15:43:10 +0530

    update profile controller

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,3 +1,4 @@
+import uploadOnCloudinary from "../config/cloudinary.js";
 import User from "../model/userModel.js"
 
 
@@ -13,4 +14,26 @@ if(!user){
 catch(error){
 return res.status(500).json({message:`get current user error ${error}`});
 }
+  }
+
+  export const updateProfile=async(req,res)=>{
+    try{
+const userId=req.userId;
+const {description,name}=req.body
+let photoUrl
+if(req.file){
+  photoUrl=await uploadOnCloudinary(req.file.path);
+
+}
+const user=await User.findByIdAndUpdate(userId,{
+  name,description,photoUrl
+})
+if(!user){
+   return res.status(404).json({message:"user not found"});
+}
+res.status(200).json(user);
+
+    }catch(error){
+return res.status(500).json({message:`update user profile error ${error}`});
+    }
   }
