c8781b62b589c92567792f7627361635503a3034
Hitesh Kumar Roy <hiteshroy011@gmail.com>
2025-10-07 14:46:10 +0530

    showing video preview and lectures in viewcourse

--- frontend/src/pages/ViewCourse.jsx
+++ frontend/src/pages/ViewCourse.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import pic from '../assets/empty.jpg'
-import { FaArrowLeftLong, FaStar } from 'react-icons/fa6'
+import { FaArrowLeftLong, FaStar } from 'react-icons/fa6';
+import {FaPlayCircle,FaLock} from 'react-icons/fa'
 import { useNavigate, useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { setSelectedCourse } from '../redux/courseSlice';
@@ -11,7 +12,7 @@ const {publishedCourseData}=useSelector(state=>state.course);
 const dispatch=useDispatch();
 const {courseid}=useParams();
 const {selectedCourse}=useSelector(state=>state.course);
-
+const [selectedLecture,setSelectedLecture]=useState(null)
 
 const fetchCourseData=()=>{
 
@@ -39,7 +40,7 @@ useEffect(()=>{
 
   <div className='w-full md:w-1/2 '>
 
-  <FaArrowLeftLong className=' cursor-pointer' onClick={()=>navigate('/')} size={25} onClickCapture={()=>navigate('/')} />
+  <FaArrowLeftLong className=' cursor-pointer' onClick={()=>navigate('/')} size={25}  />
 
   <img src={selectedCourse?.thumbnail || pic } className=' rounded-xl w-full  ' alt="" />
 
@@ -64,6 +65,70 @@ useEffect(()=>{
   </div>
 </div>
 
+<div>
+  <h2 className='text-xl font-semibold mb-2'> What You'll Learn</h2>
+  <ul className='list-disc pl-6 text-gray-700 space-y-1'>
+    <li>Learn {selectedCourse?.category} from Beginning</li>
+  </ul>
+</div>
+<div>
+  <h2 className='text-xl font-semibold mb-2'>Who This Course Is For</h2>
+  <p className='text-gray-700'> Beginners, aspiring developers, and professionals looking to upgrade skills.</p>
+</div>
+<div className='flex flex-col md:flex-row gap-6'>
+
+<div className='bg-white w-full md:w-2/5 p-6 rounded-2xl shadow-lg border border-gray-200'>
+
+<h2 className='text-xl font-bold mb-1 text-gray-800'>course Curriculum</h2>
+<p className='text-sm text-gray-500 mb-4'>{selectedCourse?.lectures?.length} Lectures</p>
+
+<div className='flex flex-col gap-3'>
+
+  {selectedCourse?.lectures?.map((lecture,index)=>{
+if(lecture.isPreviewFree)
+    return <button key={index} disabled={!lecture.isPreviewFree} className={`flex items-center gap-3 px-4 py-3 border rounded-lg transition-all duration-200 text-left ${selectedLecture?.lectureTitle==lecture.lectureTitle && "bg-gray-100 border-gray-400"}  ${lecture.isPreviewFree?"border-gray-300 cursor-pointer hover:bg-gray-100":"border-gray-200 opacity-60 cursor-not-allowed"} `} 
+
+onClick={(e)=>{
+  e.preventDefault();
+    console.log("hii");
+      if(lecture.isPreviewFree){
+      
+setSelectedLecture(lecture);
+console.log(selectedLecture)
+    }
+  }
+}
+    
+    >
+      <span className='text-lg text-gray-700'><FaPlayCircle/></span>
+      <span className='text-gray-800 text-sm font-medium'>      {lecture.lectureTitle}</span>
+</button>
+  })}
+  {selectedCourse?.lectures?.map((lecture,index)=>{
+if(!lecture.isPreviewFree)
+    return <button key={index} disabled={lecture.isPreviewFree} className={`flex items-center gap-3 px-4 py-3 border rounded-lg transition-all duration-200 text-left  ${lecture.isPreviewFree?"border-gray-300 cursor-pointer hover:bg-gray-100":"border-gray-200 opacity-60 cursor-not-allowed"} `} onClick={()=>{
+      if(lecture.isPreviewFree){
+setSelectedLecture(lecture);
+    }}}>
+        <span className='text-lg text-gray-700'><FaLock/></span>
+     <span className='text-gray-800 text-sm font-medium'>      {lecture.lectureTitle}</span></button>
+  })}
+
+  
+</div>
+</div>
+
+<div className='bg-white w-full md:w-3/5 p-6 rounded-2xl shadow-lg border border-gray-200'
+>
+  <div className='aspect-video w-full rounded-lg overflow-hidden mb-4 bg-black flex items-center justify-center '>
+    {selectedLecture?.videoUrl?<video 
+    className='w-full h-full object-cover' src={selectedLecture?.videoUrl} controls/>:<span 
+    className='text-white text-sm'>Select a preview lecture to watch</span>}
+  </div>
+
+</div>
+
+</div>
     </div>
     </div>
   )
